e4c1777c7d5289d31f99aeaf6bf0ed015a54d56d
lighter <apple.xva@gmail.com>
2021-05-08 17:20:53 +0800

    first commit

---
+++ src/main/java/com/example/myspring5webapp/MySpring5webappApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpring5webappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpring5webappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myspring5webapp/MySpring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myspring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpring5webappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
