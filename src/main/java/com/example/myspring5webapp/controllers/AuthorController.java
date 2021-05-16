b76e185e5553955f77f7cede159ff18f1e802669
lighter <apple.xva@gmail.com>
2021-05-16 19:12:44 +0800

    add author controller list view

---
+++ src/main/java/com/example/myspring5webapp/controllers/AuthorController.java
@@ -0,0 +1,23 @@
+package com.example.myspring5webapp.controllers;
+
+import com.example.myspring5webapp.repository.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+}
