55b7cd7ea424c4645cd41789dfe11f4745df99be
lighter <apple.xva@gmail.com>
2021-05-16 18:04:58 +0800

    add h2 console config, publisher, book

--- src/main/java/com/example/myspring5webapp/domain/Book.java
+++ src/main/java/com/example/myspring5webapp/domain/Book.java
@@ -14,6 +14,9 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
              inverseJoinColumns = @JoinColumn(name = "author_id"))
@@ -25,6 +28,22 @@ public class Book {
     public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     public String getTitle() {
