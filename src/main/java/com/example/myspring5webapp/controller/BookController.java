f48572a67e15049a91be8e86a37bcf0f55ddf392
lighter <apple.xva@gmail.com>
2021-05-16 18:30:57 +0800

    add bookcontroller

---
+++ src/main/java/com/example/myspring5webapp/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.example.myspring5webapp.controller;
+
+import com.example.myspring5webapp.repository.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books");
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookRepository.findAll());
+        
+        return "books";
+    }
+}
